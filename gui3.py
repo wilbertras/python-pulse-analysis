34604a54ea4aff57a59c25b7e601bb1528900ff4
wilbertr <125577618+wilbertr@users.noreply.github.com>
2024-05-06 15:52:59 +0200

    fix if no pulses

--- gui3.py
+++ gui3.py
@@ -134,25 +134,27 @@ class GUI:
                 pw = int(pw / dt)
                 pulses = []
                 offset = int(np.ceil(int(25 / dt)))
+                too_close = 0
                 for i, loc in enumerate(locs):
-                    too_close = 0
                     if  i < nr_peaks-1 and loc + pw + offset >= locs[i+1] and loc + pw <= nr_points and loc - offset >= 0:
                         too_close += 1
                     else:
                         pulses.append(X[loc-offset:loc+pw])
                 if too_close:
                     print('%d peaks too close' % too_close)
+                
                 pulses = np.array(pulses).reshape((-1, pw+offset))
-                mean_pulse = np.mean(pulses, axis=0)
-                ax = axes['c']
-                t_pulse = np.linspace(-offset*dt, pw*dt, len(mean_pulse))
-                ax.plot(t_pulse, mean_pulse)
-                ax.set_xlabel('time [$\mu s$]')
-                ax.set_xlim([t_pulse[0], t_pulse[-1]])
-                inset = ax.inset_axes([.5, .5, .45, .45])
-                inset.semilogy(t_pulse, mean_pulse)
-                inset.set_xlim([t_pulse[0], t_pulse[-1]])
-                ax.set_title('mean pulse shape')
+                if nr_peaks:
+                    mean_pulse = np.mean(pulses, axis=0)
+                    ax = axes['c']
+                    t_pulse = np.linspace(-offset*dt, pw*dt, len(mean_pulse))
+                    ax.plot(t_pulse, mean_pulse)
+                    ax.set_xlabel('time [$\mu s$]')
+                    ax.set_xlim([t_pulse[0], t_pulse[-1]])
+                    inset = ax.inset_axes([.5, .5, .45, .45])
+                    inset.semilogy(t_pulse, mean_pulse)
+                    inset.set_xlim([t_pulse[0], t_pulse[-1]])
+                    ax.set_title('mean pulse shape')
             plt.show()
 
 
